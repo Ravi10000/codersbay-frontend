0b0a1c100016b67792529f69498b5764a1dc6219
ravi <pabhinav400@gmail.com>
2023-05-01 18:28:04 +0530

    fix register form

--- src/components/RegisterImg/Register.jsx
+++ src/components/RegisterImg/Register.jsx
@@ -1,47 +1,45 @@
-import React from 'react'
-import './Register.css';
-import registerImg from './registerImage.png';
+import styles from "./Register.module.scss";
+import TextInput from "../text-input/text-input";
+import registerImg from "./registerImage.png";
+import CustomTextarea from "../custom-textarea/custom-textarea";
 const Register = () => {
-    return (
-        <div className='register'>
-            <div className='registerCenterDiv'>
-                <div className='registerImg'>
-                    <div className='registerText'>
-                        <p className='registerHead1'>Let's talk about your project</p>
-                        <p className='registerHead2'>Lets make your hunt easier</p>
-                    </div>
-                    <img className='registerActualImg' src={registerImg} alt="RegisterImage" />
-                </div>
-                <form className='regsiterForm'>
-                    <form className='fields'>
-                        <div className='f'>
-                            <div className='field'>
-                                <p className='lbl'>First Name</p>
-                                <input type="text" placeholder='Enter first name' className='inputField' />
-                            </div>
-                            <div className='field'>
-                                <p className='lbl'>Last Name</p>
-                                <input type="text" placeholder='Enter last name' className='inputField' />
-                            </div>
-                        </div>
-                        <div className='fieldTwo'>
-                            <p className='lbl'>Phone Number</p>
-                            <input type="text" placeholder='Enter phone number' className='inputFieldTwo' />
-                        </div>
-                        <div className='fieldTwo'>
-                            <p className='lbl'>Enter Your Email</p>
-                            <input type="text" placeholder='Enter email address' className='inputFieldTwo' />
-                        </div>
-                        <div className='fieldTwo'>
-                            <p className='lbl'>Message</p>
-                            <input type="text" placeholder='Message' className='inputFieldThree' />
-                        </div>
-                    </form>
-                    <button className='getQuoteButton'><p className='getQuoteText'>Get Quote</p></button>
-                </form>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.registerSection}>
+      <h2 className={styles.formTitle}>Let's talk about your project</h2>
 
-export default Register
+      <div className={styles.mainContainer}>
+        <div className={styles.heroSection}>
+          <div className={styles.leftSection}>
+            <h2 className={styles.title}>Let's talk about your project</h2>
+            <p className={styles.subtitle}>Lets make your hunt easier</p>
+          </div>
+          {/* <img
+            className={styles.heroBg}
+            src="/register-form-hero.png"
+            alt="Register"
+          /> */}
+        </div>
+        <form className={styles.registerForm}>
+          <div className={styles.nameInputs}>
+            <TextInput label="First Name" placeholder="Enter First Name" />
+            <TextInput label="Last Name" placeholder="Enter Last Name" />
+          </div>
+          <TextInput
+            label="Email"
+            type="email"
+            placeholder="Enter Email Address"
+          />
+          <TextInput
+            label="Phone Number"
+            type="number"
+            placeholder="Enter Phone Number"
+          />
+          <CustomTextarea label="Message" />
+          <button className={styles.submitBtn}>Get Quote</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
